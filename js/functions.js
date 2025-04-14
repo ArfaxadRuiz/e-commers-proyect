2fd08fc38d24e23b579a6c78e54f44d1824b8569
ArfaxadRuiz <iron_arfa@hotmail.com>
2025-04-14 09:58:30 -0600

    se agrga funcionalidad al boton de menu y se agrgea boton de agreagr al carrito en los disfraces

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     const btnCarrito = document.getElementById("btnCarrito");
     const carrito = document.querySelector(".main__carr");
+
+    const btnMenu = document.getElementById("btnMenu");
+    const menu = document.querySelector(".main__rightMenu");
+    const btnCerrarMenu = document.querySelector(".main__rightMenu--close");
 
     btnCarrito.addEventListener("click", function () {
         // Verificamos si el carrito está visible
@@ -13,5 +18,21 @@ document.addEventListener("DOMContentLoaded", function () {
             carrito.style.right = "0"; // Mostrar
             console.log("Carrito mostrado");
         }
+    });
+
+    btnMenu.addEventListener("click", function () {
+        const menuLeft = menu.style.left;
+        if (menuLeft === "0px" || menuLeft === "0") {
+            menu.style.left = "-100%";
+            console.log("Menú ocultado");
+        } else {
+            menu.style.left = "0";
+            console.log("Menú mostrado");
+        }
+    });
+
+    btnCerrarMenu.addEventListener("click", function () {
+        menu.style.left = "-100%";
+        console.log("Menú cerrado");
     });
 });
